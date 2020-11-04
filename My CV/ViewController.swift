8a0cb74226ffe1145117fa31f7f610700f923f8a
Airks <emilio.del-castillo@outlook.com>
2020-11-04 16:16:28 +0100

    Initial Commit

---
+++ My CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My CV
+//
+//  Created by Emilio Del Castillo on 04/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
